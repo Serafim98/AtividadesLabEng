707d94d8ffdb5f97b4f5dc30c8928725a68d989f
Luis Serafim <33844318+Serafim98@users.noreply.github.com>
2022-08-17 12:02:49 -0300

    Implementada função de reiniciar jogo
    Falta enviar o valor das chances para a div

--- Desafio.js
+++ Desafio.js
@@ -1,36 +1,46 @@
 let aleatorio = Math.floor(Math.random() * 101);
 let palpites = new Array();
+let chances = 10;
 
 function enviarPalpite() {
-
+    chances--;
     let palpite = document.getElementById("palpite");
-
+    let histPalpites = document.getElementById("histPalpites")
     let divResultado = document.getElementById("resultado");
 
     palpites.push(palpite.value)
-    divResultado.innerHTML = palpites
+    histPalpites.innerHTML = palpites
 
     if (palpite.value == aleatorio) {
 
-        alert("Ganhou");
-        document.location.reload(true);
-
+        divResultado.innerHTML = "Parabéns você ganhou"
+        document.getElementById("btn_Palpitar").disabled = true;
     }
 
     else if (palpite.value > aleatorio) {
 
-        alert("Número muito grande");
+        divResultado.innerHTML = "Palpite muito alto";
 
     }
 
     else if (palpite.value < aleatorio) {
 
-        alert("Número muito pequeno");
+        divResultado.innerHTML = "Palpite muito baixo";
 
+    }
+
+    alert(chances)
+    document.getElementById("chances")= chances;
+    if(chances == 0){
+        divResultado.innerHTML = "Chances Esgotadas";
+        document.getElementById("btn_Palpitar").disabled = true;
     }
 
 
 
+}
 
+function reiniciar(){
+    document.location.reload(true);
 }
 
